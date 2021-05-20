368c6dfce00807535533d8025d6e9aa60a9315f0
bashobbel <bashobbel@gmail.com>
2021-05-20 21:25:28 +0200

    Add project files.

---
+++ TextcolorEntry/TextcolorEntry/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TextcolorEntry
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void HandlePlay_Clicked(object sender, EventArgs e)
+        {
+            SwipeAnimation.PlayAnimation();
+        }
+
+        void HandlePause_Clicked(object sender, EventArgs e)
+        {
+            SwipeAnimation.PlayAnimation();
+        }
+    }
+}
